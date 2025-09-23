4e7f1b8af829352406546f73cc84a662218512bb
heet <heet.kartik.talati@gmail.com>
2025-09-23 17:32:49 -0600

    Totally normal website

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,9 +1,10 @@
 export default function Item({ name, quantity, category }) {
   return (
     <>
-      <li className="border rounded-sm p-2">
+      <li className="border rounded-sm p-2 | animate bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 border-gray-100">
         <p className="">{name}</p>
         <p className="">Quantity: {quantity}</p>
+
         <p className="">
           Category:{" "}
           {category
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -7,9 +7,11 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <main className="mx-auto max-w-xl p-4 flex flex-col">
-      <h1 className="text-2xl font-bold">Shopping List</h1>
-      <ItemList></ItemList>
-    </main>
+    <div className="bg-[url('https://i.gifer.com/QFK.gif')] bg-auto animate-pulse">
+      <main className="mx-auto max-w-xl p-4 flex flex-col bg-[url('https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWhtN3M0ZmJnbHdkd3BlYnRnMzJ1cjl4NWpqcjd2YXRjNXA1ZHpoeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6Ztl4SjQ3eeudkkw/giphy.gif')] bg-contain bg-center animate-spin">
+        <h1 className="text-2xl font-bold">Shopping List</h1>
+        <ItemList></ItemList>
+      </main>
+    </div>
   );
 }
