d45c3f145149ecdc73099146e647f84f477eabed
heet <heet.kartik.talati@gmail.com>
2025-10-09 00:24:26 -0600

    Week-5 setup

--- app/page.js
+++ app/page.js
@@ -10,6 +10,8 @@ export default function Home() {
       <Link href="/week-3">Go to Week 3</Link>
       <span> → </span>
       <Link href="/week-4">Go to Week 4</Link>
+      <span> → </span>
+      <Link href="/week-5">Go to Week 5</Link>
     </>
   );
 }
